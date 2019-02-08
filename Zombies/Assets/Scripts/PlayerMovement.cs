16c79ab41834776cbb8f87e43c759fdbb50a1c4c
Daniyal Vemuri <daniyal.vemuri@gmail.com>
2019-02-08 19:10:55 +0000

    Movement and animations added
    TODO Link animations to movements

---
+++ Zombies/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float walkSpeed;
+    private float curSpeed;
+    private float maxSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+        float sprintSpeed = walkSpeed + (walkSpeed / 2);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        curSpeed = walkSpeed;
+        maxSpeed = curSpeed;
+
+        // Move senteces
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * curSpeed, 0.8f),
+                                             Mathf.Lerp(0, Input.GetAxis("Vertical") * curSpeed, 0.8f));
+    }
+}
